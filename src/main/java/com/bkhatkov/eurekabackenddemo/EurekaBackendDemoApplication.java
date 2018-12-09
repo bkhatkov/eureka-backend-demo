048b24b540e1f7d4f10651e2c958fe2674be5307
Boris Khatkov <bkhatkov@gmail.com>
2018-12-09 23:31:52 +0300

    Initial commit

---
+++ src/main/java/com/bkhatkov/eurekabackenddemo/EurekaBackendDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bkhatkov.eurekabackenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaBackendDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaBackendDemoApplication.class, args);
+    }
+}
